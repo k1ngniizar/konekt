d9e226ec3a61bb87f86bcf16d37b2e49b61dcc44
k1ngniizar <huncleniizar@gmail.com>
2025-10-27 15:22:03 +0100

    deployment

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,6 +9,7 @@ const genTokenSetCookie = (userId, res)=>{
     maxAge: 15 * 24 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development"
   })
 }
 
