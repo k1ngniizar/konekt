b46c8eab7407245be756820b957bab3455806630
k1ngniizar <huncleniizar@gmail.com>
2025-11-05 22:01:44 +0100

    Socket error fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/socketContext.jsx'
+import { SocketContextProvider } from './context/SocketContext.jsx'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
